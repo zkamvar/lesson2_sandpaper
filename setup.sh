48aeeb60b9c1041e0dba02953bec614ef4385d49
sabaferdous12 <saba.ferdous.13@ucl.ac.uk>
2022-02-11 17:47:26 +0000

    Forced python environment location at home dir

--- setup.sh
+++ setup.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/bash
 
-python -m venv r-env
-source r-env/bin/activate
+
+mkdir -p ~/.virtualenvs/
+
+python3 -m venv ~/.virtualenvs/r-env
+source ~/.virtualenvs/r-env/bin/activate
 
 pip install -r requirements.txt
