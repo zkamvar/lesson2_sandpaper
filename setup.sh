c16c288bf4ac5b06fab2142edd5c87fc6dd491a8
Saba Ferdous <saba.ferdous.13@ucl.ac.uk>
2022-02-17 19:55:17 +0000

    Adding Sudo2

--- setup.sh
+++ setup.sh
@@ -8,4 +8,6 @@ source ~/.virtualenvs/r-env/bin/activate
 
 pip install -r requirements.txt
 
+sudo apt update -y 
+
 sudo apt install -y texlive-latex-base texlive-fonts-recommended texlive-fonts-extra texlive-latex-extra
